698ffbfac7ebea73bc60a64811af5ad335640ca0
Andrey Matushenko <andrii.matiushenko@ardas.dp.ua>
2019-08-21 00:57:52 +0300

    Add more ugly things

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React, { Fragment, Component, useState } from "react";
+import React, { Fragment } from "react";
 import axios from "axios";
 import {
   SafeAreaView,
@@ -16,139 +16,143 @@ import {
   TextInput,
   Text,
   View,
-  Modal,
+  Picker,
   TouchableOpacity
 } from "react-native";
 
-import { createStore, combineReducers } from "redux";
-import { connect, Provider } from "react-redux";
-
-function results(state = { list: [], selected: {} }, action) {
-  switch (action.type) {
-    case "set results": {
-      return { ...state, list: [...action.data] };
-    }
-    case "select": {
-      return { ...state, selected: { ...action.data } };
-    }
-    default:
-      return state;
+const search = (byRepo, byUser, byIssues, text) => {
+  if (byRepo) {
+    return axios.get(`https://api.github.com/search/repositories?q=${text}`);
   }
-}
-
-function modal(state, action) {
-  switch (action.type) {
-    case "toggle": {
-      return !state;
-    }
-    default:
-      return state;
+  if (byUser) {
+    return axios.get(`https://api.github.com/search/users?q=${text}`);
   }
-}
+  if (byIssues) {
+    return axios.get(`https://api.github.com/search/issues?q=${text}`);
+  }
+};
 
-/// remove redux. use component state currently, then forceUpdate, & set value inside this, then setState({value:!value})
-let store = createStore(combineReducers({ results, modal }), {});
-
-/// merger in one component, remove modal, use results&&
-const Details = connect(state => ({
-  selected: state.results.selected,
-  visible: state.modal
-}))(
-  ({ selected, visible }) =>
-    console.log("selected", selected) || (
-      <Modal visible={visible}>
-        <View
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
-          <Text>Details</Text>
-          <Text>Score : {selected.score}</Text>
-          <Text>
-            Forks? : {selected.fork && selected.fork ? "true" : "false"}
-          </Text>
-          <Text>Full Name : {selected.full_name}</Text>
-          <TouchableOpacity
-            onPress={() => store.dispatch({ type: "toggle" })}
-            style={{ paddingTop: 50 }}
-          >
-            <Text>Hide</Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
-    )
-);
-
-const Body = connect(state => ({ results: state.results.list }))(
-  ({ results }) => (
-    <Fragment>
-      <StatusBar barStyle="dark-content" backgroundColor="lime" />
-      <SafeAreaView style={{ flex: 1, backgroundColor: "lime" }}>
-        <View
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "lime"
-          }}
-        >
-          <Text
+class App extends React.Component {
+  state = {};
+  render() {
+    const { results, selected } = this.state;
+    return React.cloneElement(
+      <Fragment>
+        <StatusBar barStyle="dark-content" backgroundColor="lime" />
+        <SafeAreaView style={{ flex: 1, backgroundColor: "lime" }}>
+          <View
             style={{
-              fontWeight: "bold",
-              fontSize: 35
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "lime"
             }}
           >
-            Best Git Repo searcher
-          </Text>
-        </View>
-        <Text> Search some repositories</Text>
-        <TextInput
-          onChangeText={text =>
-            axios
-              /// add util to switch search entity with ugly signature
-              .get(`https://api.github.com/search/repositories?q=${text}`)
-              .then(res => {
-                console.log({ res });
-                store.dispatch({ type: "set results", data: res.data.items });
-              })
-          }
-        />
-        <Details />
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}
-        >
-          {results.map(item => (
-            <View
+            <Text
               style={{
-                backgroundColor: "aquamarine",
-                borderBottomColor: "black",
-                borderBottomWidth: 1
+                fontWeight: "bold",
+                fontSize: 35
               }}
             >
-              <Text>{item.name}</Text>
+              Best Git Repo searcher
+            </Text>
+          </View>
+          <Text> Search some repositories</Text>
+          <TextInput
+            style={{ borderColor: "black", borderWidth: 1 }}
+            onChangeText={text =>
+              search(
+                this.state.searchBy == "repositories",
+                this.state.searchBy == "users",
+                this.state.searchBy == "issues",
+                text
+              )
+                .then(res => {
+                  console.log({ res });
+                  this.state.results = res.data.items;
+                  this.forceUpdate();
+                })
+                .catch(err => console.log({ err }))
+            }
+          />
+          {console.log("state", this.state)}
+          <Picker
+            selectedValue={this.state.searchBy || "repositories"}
+            style={{
+              height: 150,
+              width: 100
+            }}
+            onValueChange={itemValue => {
+              this.state.searchBy = itemValue;
+              this.forceUpdate();
+            }}
+          >
+            <Picker.Item
+              color="black"
+              label="repositories"
+              value="repositories"
+            />
+            <Picker.Item color="black" label="users" value="users" />
+            <Picker.Item color="black" label="issues" value="issues" />
+          </Picker>
+          {Object.keys(selected || {}).length > 0 ? (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Text>Details</Text>
+              <Text>Score : {selected.score}</Text>
+              <Text>
+                Forks? : {selected.fork && selected.fork ? "true" : "false"}
+              </Text>
+              <Text>Full Name : {selected.full_name}</Text>
               <TouchableOpacity
                 onPress={() => {
-                  store.dispatch({ type: "toggle" });
-                  store.dispatch({ type: "select", data: item });
+                  delete this.state.selected;
+                  this.setState({ changed: !this.state.changed });
                 }}
+                style={{ paddingTop: 50 }}
               >
-                <Text
-                  style={{ textDecorationLine: "underline", color: "blue" }}
-                >
-                  {item.url}
-                </Text>
+                <Text>Hide</Text>
               </TouchableOpacity>
             </View>
-          ))}
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  )
-);
-
-const App = () => (
-  <Provider store={store}>
-    <Body />
-  </Provider>
-);
+          ) : (
+            <ScrollView
+              contentInsetAdjustmentBehavior="automatic"
+              style={styles.scrollView}
+            >
+              {(results || []).map(item => (
+                <View
+                  style={{
+                    backgroundColor: "aquamarine",
+                    borderBottomColor: "black",
+                    borderBottomWidth: 1
+                  }}
+                >
+                  <Text>{item.name}</Text>
+                  <TouchableOpacity
+                    onPress={() => {
+                      this.state.selected = item;
+                      this.forceUpdate();
+                    }}
+                  >
+                    <Text
+                      style={{ textDecorationLine: "underline", color: "blue" }}
+                    >
+                      {item.url}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              ))}
+            </ScrollView>
+          )}
+        </SafeAreaView>
+      </Fragment>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   scrollView: {
